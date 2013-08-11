99113fe26a7639935af60ed5eaa6198d4a4c9023
Ivan Krasin <imkrasin@gmail.com>
2013-08-11 13:33:37 -0700

    Add a simple program that connects to tun2, listens for IP packets and dumps them

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"log"
+	"os"
+	"syscall"
+	"unsafe"
+)
+
+const (
+	IFNAMSIZ = 16
+
+	IFF_NO_PI = 0x1000
+	IFF_TUN   = 0x0001
+)
+const TUNSETIFF = 1074025674
+
+type ifreq struct {
+	name  [IFNAMSIZ]byte
+	flags uint16
+}
+
+func ioctl(fd uintptr, req int, data unsafe.Pointer) (err syscall.Errno) {
+	_, _, err = syscall.RawSyscall(syscall.SYS_IOCTL, fd, uintptr(req), uintptr(data))
+	return
+}
+
+func main() {
+	tun, err := os.OpenFile("/dev/net/tun", os.O_RDWR, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Opened tun device, f: %v\n", tun.Fd())
+
+	req := ifreq{
+		name:  [16]byte{'t', 'u', 'n', '2', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		flags: IFF_TUN | IFF_NO_PI,
+	}
+	errno := ioctl(tun.Fd(), TUNSETIFF, unsafe.Pointer(&req))
+	if errno != 0 {
+		log.Fatalf("Failed to ioctl tun device, errno: %d", errno)
+	}
+	fmt.Printf("ioctl succeeded!\n")
+
+	buf := make([]byte, 4096)
+	for {
+		n, err := tun.Read(buf)
+		if err != nil {
+			log.Fatal("Error reading from tun fd: %v", err)
+		}
+		toshow := n
+		suffix := ""
+		if toshow > 200 {
+			toshow = 200
+			suffix = "... <truncated> ...\n"
+		}
+		log.Printf("Read %d bytes from tun:\n%s%s", n, hex.Dump(buf[:toshow]), suffix)
+	}
+}
